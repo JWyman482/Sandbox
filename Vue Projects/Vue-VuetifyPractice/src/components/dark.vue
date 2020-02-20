9a90236766fdc6ad0b20ba0cdf6a39b36d8f9b58
JWyman482 <jwyman4821@gmail.com>
2020-02-20 07:03:20 -0800

    Cleanup and add cards

--- Vue Projects/Vue-VuetifyPractice/src/components/dark.vue
+++ Vue Projects/Vue-VuetifyPractice/src/components/dark.vue
@@ -9,7 +9,7 @@
  
             <div class="" style="width: 500px; margin: auto;">
                 <div>
-                    <span>Click a selection below</span>
+                    
                     <v-tabs color="green" v-model="tab" vertical>
                         <v-tab v-for="item in items" :key="item.tab">
                             {{item.tab}}
@@ -18,7 +18,12 @@
                             <v-tab-item v-for="item in items" :key="item.tab">
                                 <v-card flat>
                                     <v-card-text>
-                                        {{item.content}}
+                                        <v-text-field v-show="tab!=0" label="Requested ID" single-line v-model="item_id">
+                                        </v-text-field>
+                                        <v-text-field v-show="tab==0 || tab==3" label="Name" single-line v-model="name">
+                                        </v-text-field>
+                                        <span v-show="isSuccess">The operation was a success.</span>
+                                        <span v-show="!isSuccess">The operation failed.</span>
                                     </v-card-text>
                                     <v-card-actions>
                                         <v-btn class="btn" color="green" small dark @click="makeReq">
@@ -29,53 +34,22 @@
                             </v-tab-item>
                         </v-tabs-items>
                     </v-tabs>
-                    <div class="selectWrap">
-                        
-                    </div>
 
                 </div>
                 <v-card
+                    dark
                     max-width="344"
-                    class="mx-auto"
+                    class="mx-auto v-card"
+                    v-show="output!=null"
+                    v-for="(row, index) in output"
+                    :key="index"
                 >
-                    <v-list-item>
-                    <v-list-item-avatar color="grey"></v-list-item-avatar>
-                    <v-list-item-content>
-                        <v-list-item-title class="headline">Our Changing Planet</v-list-item-title>
-                        <v-list-item-subtitle>by Kurt Wagner</v-list-item-subtitle>
-                    </v-list-item-content>
-                    </v-list-item>
-
-                    <v-img
-                    src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg"
-                    height="194"
-                    ></v-img>
-
+                    <v-card-title>
+                        {{row.name}}
+                    </v-card-title>
                     <v-card-text>
-                    Visit ten places on our planet that are undergoing the biggest changes today.
+                        ID: {{row.item_id}}
                     </v-card-text>
-
-                    <v-card-actions>
-                    <v-btn
-                        text
-                        color="deep-purple accent-4"
-                    >
-                        Read
-                    </v-btn>
-                    <v-btn
-                        text
-                        color="deep-purple accent-4"
-                    >
-                        Bookmark
-                    </v-btn>
-                    <v-spacer></v-spacer>
-                    <v-btn icon>
-                        <v-icon>mdi-heart</v-icon>
-                    </v-btn>
-                    <v-btn icon>
-                        <v-icon>mdi-share-variant</v-icon>
-                    </v-btn>
-                    </v-card-actions>
                 </v-card>
             </div>
 
@@ -99,6 +73,7 @@
             name: null,
             item_id: null,
             output: null,
+            isSuccess: false,
 
             items: [
                 { tab: 'INSERT', content: 'Insert a name for the field' },
@@ -115,9 +90,30 @@
                 var reqdata = {};
 
                 reqdata["action"] = this.items[this.tab].tab;
+
+                // INSERT
+                if (this.tab == 0) {
+                    reqdata["name"] = this.name;
+                }
+
+                // GET
+                if (this.tab == 1 && this.item_id != null) {
+                    reqdata["id"] = "id";
+                    reqdata["item_id"] = this.item_id;
+                }
+
+                // DELETE
+                if (this.tab == 2 && this.item_id != null) {
+                    reqdata["item_id"] = this.item_id;
+                }
+
+                // UPDATE
+                if (this.tab == 3 && this.item_id != null) {
+                    reqdata["item_id"] = this.item_id;
+                    reqdata["name"] = this.name;
+                }
                 // reqdata["name"] = this.name;
                 // reqdata["id"] = this.itemid;
-
                 console.log(reqdata);
 
                 const params = {
@@ -130,10 +126,19 @@
                         return data.json();
                     })
                     .then(res => {
-                        this.output = res;
+                        console.log(res);
+                        if (res.affectedRows && res.affectedRows > 0) {
+                            this.output = null;
+                            this.isSuccess = true;
+                        }
+                        else {
+                            this.output = res;
+                            this.isSuccess = false;
+                        }
                     })
 
                     .catch(error => this.output = error)
+                    this.item_id = null;
             }
         }
     }
@@ -159,5 +164,8 @@
     }
     .names {
         text-align: left;
+    }
+    .v-card {
+        margin: 20px;
     }
 </style>
