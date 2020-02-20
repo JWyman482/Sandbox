f44e77af4b0be03640fffd027db07f2eee194b5c
JWyman482 <jwyman4821@gmail.com>
2020-02-20 05:10:33 -0800

    Adjusting cards

--- Vue Projects/Vue-VuetifyPractice/src/components/dark.vue
+++ Vue Projects/Vue-VuetifyPractice/src/components/dark.vue
@@ -15,29 +15,68 @@
                             {{item.tab}}
                         </v-tab>
                         <v-tabs-items v-model="tab">
-                                <v-card flat style="justify-content: center">
-                                    <v-tab-item v-for="item in items" :key="item.tab">
-                                            <v-textarea :placeholder="item.content" v-model="name"/>
-                                    </v-tab-item>
+                            <v-tab-item v-for="item in items" :key="item.tab">
+                                <v-card flat>
+                                    <v-card-text>
+                                        {{item.content}}
+                                    </v-card-text>
+                                    <v-card-actions>
+                                        <v-btn class="btn" color="green" small dark @click="makeReq">
+                                            Submit
+                                        </v-btn>
+                                    </v-card-actions>
                                 </v-card>
+                            </v-tab-item>
                         </v-tabs-items>
                     </v-tabs>
                     <div class="selectWrap">
-                        <v-btn class="btn" color="green" small dark @click="makeReq">
-                            Submit
-                        </v-btn>
+                        
                     </div>
-                {{output}}
+
                 </div>
-                <table>
-                    <tr class="tbl">
-                        <th>ID</th><th>Name</th>
-                    </tr>
-                    <tr class="tbl" v-for="(rows, index) in output" :key="index">
-                        <td>{{rows.item_id}}</td>
-                        <td class="names">{{rows.name}}</td>
-                    </tr>
-                </table>
+                <v-card
+                    max-width="344"
+                    class="mx-auto"
+                >
+                    <v-list-item>
+                    <v-list-item-avatar color="grey"></v-list-item-avatar>
+                    <v-list-item-content>
+                        <v-list-item-title class="headline">Our Changing Planet</v-list-item-title>
+                        <v-list-item-subtitle>by Kurt Wagner</v-list-item-subtitle>
+                    </v-list-item-content>
+                    </v-list-item>
+
+                    <v-img
+                    src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg"
+                    height="194"
+                    ></v-img>
+
+                    <v-card-text>
+                    Visit ten places on our planet that are undergoing the biggest changes today.
+                    </v-card-text>
+
+                    <v-card-actions>
+                    <v-btn
+                        text
+                        color="deep-purple accent-4"
+                    >
+                        Read
+                    </v-btn>
+                    <v-btn
+                        text
+                        color="deep-purple accent-4"
+                    >
+                        Bookmark
+                    </v-btn>
+                    <v-spacer></v-spacer>
+                    <v-btn icon>
+                        <v-icon>mdi-heart</v-icon>
+                    </v-btn>
+                    <v-btn icon>
+                        <v-icon>mdi-share-variant</v-icon>
+                    </v-btn>
+                    </v-card-actions>
+                </v-card>
             </div>
 
         </v-content>
@@ -73,11 +112,9 @@
                 const url = 'https://5261puzqsb.execute-api.us-west-2.amazonaws.com/default';
                 //const key = 'szqaVuropf37NRM8CgdhH3ekEynhYaEewYYz36Qb';
 
-                var reqdata = {
-                    "action": ""
-                };
+                var reqdata = {};
 
-                reqdata["action"] = this.items[this.tab].tab.toLowerCase();
+                reqdata["action"] = this.items[this.tab].tab;
                 // reqdata["name"] = this.name;
                 // reqdata["id"] = this.itemid;
 
