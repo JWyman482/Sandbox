6f00e9e5f79d7a75ac348a4353d18f0e9111f9b1
JWyman482 <jwyman4821@gmail.com>
2020-11-19 06:01:01 -0800

    added shadow

--- Vue Projects/simpletask/src/components/Task.vue
+++ Vue Projects/simpletask/src/components/Task.vue
@@ -43,6 +43,11 @@ export default {
         padding: 2px;
         border: 1px solid lightgray;
     }
+
+    .task:hover {
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    }
+    
     .taskLeft {
         display: flex;
         flex-wrap: nowrap;
